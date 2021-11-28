7062faec56cfc467f8488f6c1a37fab14da69fe5
Ciclo3-P55 <email>
2021-11-28 13:32:41 -0500

    Proyecto LibrApp Cambios Compilacion Mateo

--- app/src/main/java/com/example/librapp/MainActivity.java
+++ app/src/main/java/com/example/librapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.MenuPrincipal);
+        setContentView(R.layout.menu_principal);
     }
 }
